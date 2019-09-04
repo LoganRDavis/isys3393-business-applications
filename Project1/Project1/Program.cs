45c1b25832af8f6a36d2e4427801e7309c8bdb9e
loganrickydavis <kelmer1234@gmail.com>
2019-09-04 12:17:10 -0500

    initial

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********MENU**********");
+            Console.WriteLine("**********MENU**********");
+            Console.ReadLine();
+        }
+    }
+}
